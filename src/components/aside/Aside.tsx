90657d78c10e4ae46c17988f7815caa2848e0629
Arseniy <arseniy.bendyukov2006@gmail.com>
2021-09-25 21:12:58 +0300

    create aside links

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -4,9 +4,11 @@ import { useAppDispatch, useAppSelector } from '../../tools/hooks';
 import { getArrayOfComponents } from '../../tools/functions';
 import s from './Aside.module.scss';
 import PlaylistCreationForm from '../playlist-creation/Form';
-import PlaylistCreationButton from '../playlist-creation/Button';
+import { AsideButton, AsideLink } from './AsideActions';
 import Playlist from '../small-playlist/Playlist';
 import TransitionSkeleton from '../common/transition-skeleton/TransitionSkeleton';
+import Search from '../common/search/Search';
+import Heart from '../common/heart/Heart';
 
 
 type PropsType = {};
@@ -32,7 +34,9 @@ const Aside: React.FC<PropsType> = (props) => {
 
    return (
       <aside className={s.aside}>
-         <PlaylistCreationButton onClick={toggleIsFormShown} />
+         <SearchLink />
+         <MySongsLink />
+         <PlaylistCreationButton onclick={toggleIsFormShown} />
          <div className={s.playlists}>
             { isFetching ? 
                getArrayOfComponents(() => <TransitionSkeleton width='100%' height={15} />, 6)
@@ -46,6 +50,40 @@ const Aside: React.FC<PropsType> = (props) => {
             }
          </div>
       </aside>
+   );
+};
+
+const SearchLink: React.FC = () => {
+   return (
+      <div>
+         <AsideLink to='/search'>
+            <Search color='grey' />
+            Search
+         </AsideLink>
+      </div>
+   );
+};
+
+const MySongsLink: React.FC = () => {
+   return (
+      <div>
+         <AsideLink to='/my-songs'>
+            <Heart isLiked={false} color='grey' size={14} />
+            My Songs
+         </AsideLink>
+      </div>
+   );
+};
+
+type PlaylistCreationPropsType = { onclick: () => void };
+const PlaylistCreationButton: React.FC<PlaylistCreationPropsType> = ({ onclick }) => {
+   return (
+      <AsideButton onClick={onclick}>
+         <svg viewBox="0 0 448 448" xmlns="http://www.w3.org/2000/svg">
+            <path d="m408 184h-136c-4.417969 0-8-3.582031-8-8v-136c0-22.089844-17.910156-40-40-40s-40 17.910156-40 40v136c0 4.417969-3.582031 8-8 8h-136c-22.089844 0-40 17.910156-40 40s17.910156 40 40 40h136c4.417969 0 8 3.582031 8 8v136c0 22.089844 17.910156 40 40 40s40-17.910156 40-40v-136c0-4.417969 3.582031-8 8-8h136c22.089844 0 40-17.910156 40-40s-17.910156-40-40-40zm0 0"/>
+         </svg>
+         Create playlist
+      </AsideButton>
    );
 };
 
---
+++ src/components/aside/AsideActions.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import s from './AsideActions.module.scss';
+
+type AsideButtonPropsType = { onClick: () => void };
+
+export const AsideButton: React.FC<AsideButtonPropsType> = ({ onClick, children }) => {
+   return (
+      <button onClick={onClick} className={s.button}>
+         {children}
+      </button>
+   );
+};
+
+
+type AsideLinkPropsType = { to: string };
+
+export const AsideLink: React.FC<AsideLinkPropsType> = ({ to, children }) => {
+   return (
+      <Link to={to} className={s.link}>
+         {children}
+      </Link>
+   );
+};
