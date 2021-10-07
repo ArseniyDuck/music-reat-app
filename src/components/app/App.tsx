0f20535e1b00d881c54b861a77c8e678a952533e
Arseniy <arseniy.bendyukov2006@gmail.com>
2021-10-07 22:59:53 +0300

    create media query component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { breakpoints } from '../../tools/variables';
-import { useWindowSize } from '../../tools/hooks';
 import { Route, Switch } from 'react-router-dom';
 import s from './App.module.scss';
 import Album from '../../pages/album/Album';
@@ -8,22 +6,27 @@ import Avatar from '../avatar/Avatar';
 import Aside from '../aside/Aside';
 import Playlist from '../../pages/playlist/Playlist';
 import MobileBottom from '../mobile-bottom/MobileBottom';
+import MediaQuery from '../common/media-query/MediaQuery';
 
 
 const App = () => {
-   const windowDimensions = useWindowSize();
-
    return <>
       <div className={s.wrapper}>
-         { windowDimensions.width > breakpoints.lg && <Aside /> }
+         <MediaQuery mode='min-width' width='lg'>
+            <Aside />
+         </MediaQuery>
          <main className={s.content}>
-            { windowDimensions.width > breakpoints.lg && <Avatar /> }
+            <MediaQuery mode='min-width' width='lg'>
+               <Avatar />
+            </MediaQuery>
             <Switch>
                <Route path='/album/:albumId' component={Album} />
                <Route path='/playlist/:playlistId' component={Playlist} />
             </Switch>
          </main>
-         { windowDimensions.width <= breakpoints.lg && <MobileBottom /> }
+         <MediaQuery mode='max-width' width='lg'>
+            <MobileBottom />
+         </MediaQuery>
       </div>
    </>;
 };
