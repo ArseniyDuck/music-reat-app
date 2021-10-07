d3c4787ce15b586a4f027815400199edaa069915
Arseniy <arseniy.bendyukov2006@gmail.com>
2021-10-07 23:03:34 +0300

    create media query component

---
+++ src/components/common/media-query/MediaQuery.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useWindowSize } from '../../../tools/hooks';
+import { breakpoints } from '../../../tools/variables';
+
+type PropsType = {
+   mode: 'max-width' | 'min-width'
+   width: keyof typeof breakpoints
+};
+
+const MediaQuery: React.FC<PropsType> = (props) => {
+   const { width } = useWindowSize();
+   let checkQueryCondition: (() => boolean);
+
+   switch (props.mode) {
+      case 'max-width':
+         checkQueryCondition = () => breakpoints[props.width] >= width;
+         break;
+      case 'min-width':
+         checkQueryCondition = () => breakpoints[props.width] < width;
+         break;
+      default:
+         checkQueryCondition = () => false;
+   }
+
+   return <>
+      { checkQueryCondition() && props.children }
+   </>;
+};
+
+export default MediaQuery;
