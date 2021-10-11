e955efa1befb96e4f7eb64108996c4eae416948a
Arseniy <arseniy.bendyukov2006@gmail.com>
2021-10-11 21:34:22 +0300

    increase size of mobile buttons and links

--- src/components/mobile-song-pull-out/MobileSongPullOut.tsx
+++ src/components/mobile-song-pull-out/MobileSongPullOut.tsx
@@ -37,23 +37,23 @@ const MobileSongPullOut: React.FC<PropsType> = ({ isOpened, hide, songData, ...p
             <SongInfo {...songData} />
             <div className={s.actions}>
                <Action onClick={props.likeSong}>
-                  <Heart size={28} color='pink' isLiked={songData.isLiked} />
+                  <Heart size={33} color='pink' isLiked={songData.isLiked} />
                   <span>{songData.isLiked ? 'Dislike song' : 'Like song'}</span>
                </Action>
                <Action href={`/singer/${songData.singerId}`} isArrow={true} hide={hide}>
-                  <Headphones size={28} />
+                  <Headphones size={33} />
                   <span>Go to artist</span>
                </Action>
                <Action href={`/album/${songData.albumId}`} isArrow={true} hide={hide}>
-                  <CdDisk size={25} styles={{padding: '1.5px'}} />
+                  <CdDisk size={30} styles={{padding: '1.5px'}} />
                   <span>Go to album</span>
                </Action>
                <Action onClick={() => setIsSelectionOpened(prev => !prev)}>
-                  <Plus size={18} styles={{padding: '5px'}} />
+                  <Plus size={23} styles={{padding: '5px'}} />
                   <span>Add song to playlist</span>
                </Action>
                { songData.isInPlaylist && <Action onClick={removeSongFromPlaylist}>
-                  <Trash size={20} styles={{padding: '4px'}} />
+                  <Trash size={25} styles={{padding: '4px'}} />
                   <span>Remove song from playlist</span>
                </Action> }
                <MobilePlaylistSelection
